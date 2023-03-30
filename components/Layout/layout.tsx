c30e5a0e1f4c14075101a0a0383ba71bdd0a5786
Your Name <leonardorocha.limaa@gmail.com>
2023-03-30 19:57:37 -0300

    Estruturando a base e removendo arquivos necessários + Construindo layout base

---
+++ components/Layout/layout.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "../Footer/footer"
+import { Navbar } from "../Navbar/navbar"
+
+export const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <div>Layout</div>
+            <Footer />
+        </>
+    )
+}
