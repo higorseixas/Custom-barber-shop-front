5fb745e58277ef1bd925f6b6c86e657fd4d9a5f1
higorseixas <higor.c.seixas@gmail.com>
2023-03-31 16:37:05 -0300

    Alterando Navbar

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,27 +1,55 @@
+import Link from 'next/link'
 import styled from 'styled-components'
 
-const NavbarComponent = styled.div`
+const NavbarContainer = styled.nav`
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
-  padding: 1 em 1.2em;
-  margin-bottom: 2em;
-  background-color: red;
-  color: black;
-`
+  padding: 10px;
+  background-color: #333;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1); 
+  top: 0;
+  left: 0;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
-// const Logo = styled.image`
-// `
+const Logo = styled.img`
+  width: 70px;
+  height: 70px;
+`;
+
+const NavbarButton = styled.button`
+  padding: 10px 20px;
+  margin-left: 10px;
+  background-color: orange;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+`;
+
+const NavbarButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  margin: 0 auto;
+`;
 
 export const NavBar = () => {
     return (
-        <NavbarComponent>
-            {/* <Logo>
-                dasdasd
-            </Logo> */}
-            <p>Teste</p>
-            <p>Teste</p>
-        </NavbarComponent>
-    )
-}
+      <NavbarContainer>
+        <Logo src="/images/logo.png" alt="Logo" />
+        <NavbarButtonContainer>
+          <NavbarButton>Button 1</NavbarButton>
+          <NavbarButton>Button 2</NavbarButton>
+          <NavbarButton>Button 3</NavbarButton>
+        </NavbarButtonContainer>
+      </NavbarContainer>
+    );
+};
