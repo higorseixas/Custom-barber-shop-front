a02bbf04efa862af64429dad3b897f480dcde92d
Your Name <leonardorocha.limaa@gmail.com>
2023-04-04 20:30:31 -0300

    Configurando o next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,52 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=63072000; includeSubDomains; preload',
+	},
+	{
+		key: 'X-XSS-Protection',
+		value: '1; mode=block',
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'SAMEORIGIN',
+	},
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+]
+
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // emotion: true,
+    styledComponents: true,
+  },
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+
 }
 
 module.exports = nextConfig
