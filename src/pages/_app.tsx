10d5492e03a40814a1c880fb5df989e9520a6552
higorseixas <higor.c.seixas@gmail.com>
2023-04-23 19:34:09 -0300

    Adicionando estilos globais na aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@ import type { AppProps } from 'next/app'
 import 'reset-css';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { SidebarProvider } from '@/contexts/SidebarContext';
+import { GlobalStyle } from '.';
+import { ThemeProvider } from 'styled-components';
+import theme from '@/components/Global/theme';
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <SidebarProvider>
-        <Component {...pageProps} />
-      </SidebarProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <SidebarProvider>
+          <Component {...pageProps} />
+        </SidebarProvider>
+      </ThemeProvider>
     </AuthProvider>
   )
 }
