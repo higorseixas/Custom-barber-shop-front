a71a2db6577d8a5a01c5f43c1947442692db7cde
Your Name <leonardorocha.limaa@gmail.com>
2023-04-26 19:53:51 -0300

    Alterando de link -> botão, disparando a função para alterar o seletedContent

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -7,172 +7,106 @@ import { SideBarButton } from '../Buttons/button';
 import { PrimaryLogo } from '../Logo/primaryLogo';
 import { CalenderIcon, ClientIcon, ConfigIncon, LeftSidearrowIncon, MenuDashboardIcon, PaymentIcon, ServicesIcon } from '../Svgs';
 import { SubTitleSideBarH3 } from '../Titles/primaryTitles';
-
-
-const SidebarContainer = styled.div<{ Collapsed: boolean }>`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: ${({ Collapsed }) => (Collapsed ? "50px" : "200px")};
-  height: 100vh;
-  background-color: #f2f2f2;
-  transition: all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.1);
-  position: relative;
-
-  @media screen and (max-width: 768px) {
-    height: unset;
-  }
-`;
-
-const Line = styled.hr`
-  margin: 10px 0;
-  border: 0;
-  height: 1px;
-  background-color: #ccc;
-`;
-
-const LogoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80px;
-`;
-
-const NavContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const NavItem = styled.div<{ Collapsed: boolean }>`
-  display: flex;
-  align-items: center;
-  padding: ${({ Collapsed }) => (Collapsed ? "10px" : "10px 20px")};
-  /* padding: 10px; */
-  border-radius: 20px;
-  margin-bottom: 10px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #ccc;
-  }
-`;
-
-const NavItemIcon = styled.div<{ Collapsed: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: ${({ Collapsed }) => (Collapsed ? "center" : "flex-start")};
-  margin-right: ${({ Collapsed }) => (Collapsed ? "0" : "10px")};
-`;
-
-const UserContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  margin-top: auto;
-`;
-
-const UserAvatar = styled.img`
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  margin-right: 10px;
-`;
+import { Line, LogoContainer, NavContainer, NavItem, NavItemIcon, SidebarContainer, UserAvatar, UserContainer } from './styles';
 
 
 export default function Sidebar() {
   const router = useRouter();
-  const { isCollapsed, toggleSidebarcollapse } = useContext(SidebarContext);
+  const { isCollapsed, toggleSidebarcollapse, handleSelectedContent } = useContext(SidebarContext);
 
   const sidebarItems = [
     {
       name: "Home",
-      href: "/dashboard",
+      href: "dashboard",
       icon: <MenuDashboardIcon style={{
-              marginRight:"10px", 
-              height:"30px", 
-              width: "30px" 
-            }}/>,
+        marginRight: "10px",
+        height: "30px",
+        width: "30px"
+      }} />,
     },
     {
       name: "Caixa",
-      href: "/caixa",
+      href: "caixa",
       icon: <PaymentIcon style={{
-              marginRight:"10px", 
-              height:"30px", 
-              width: "30px" 
-            }}/>,
+        marginRight: "10px",
+        height: "30px",
+        width: "30px"
+      }} />,
     },
     {
       name: "Agendamento",
-      href: "/agendamento",
+      href: "agendamento",
       icon: <CalenderIcon style={{
-              marginRight:"10px", 
-              height:"30px", 
-              width: "30px" 
-            }}/>,
+        marginRight: "10px",
+        height: "30px",
+        width: "30px"
+      }} />,
     },
     {
       name: "Clientes",
-      href: "/clientes",
+      href: "clientes",
       icon: <ClientIcon style={{
-              marginRight:"10px", 
-              height:"30px", 
-              width: "30px" 
-            }}/>,
+        marginRight: "10px",
+        height: "30px",
+        width: "30px"
+      }} />,
     },
     {
       name: "Produtos &\n Serviços",
-      href: "/produtosEServiços",
+      href: "produtosEServiços",
       icon: <ServicesIcon style={{
-              marginRight:"10px", 
-              height:"30px", 
-              width: "30px" 
-            }}/>,
+        marginRight: "10px",
+        height: "30px",
+        width: "30px"
+      }} />,
     },
     {
       name: "Configurações",
-      href: "/configurações",
+      href: "configurações",
       icon: <ConfigIncon style={{
-              marginRight:"10px", 
-              height:"30px", 
-              width: "30px" 
-            }}/>,
+        marginRight: "10px",
+        height: "30px",
+        width: "30px"
+      }} />,
     },
   ];
+
+  const handleChangeContent = (content: string) => {
+    handleSelectedContent(content)
+  }
 
   return (
     <SidebarContainer Collapsed={isCollapsed}>
       <SideBarButton onClick={toggleSidebarcollapse}>
-        {!isCollapsed ? <LeftSidearrowIncon style={{transform: "scaleX(-1)"}} /> : <LeftSidearrowIncon/>}
+        {!isCollapsed ? <LeftSidearrowIncon style={{ transform: "scaleX(-1)" }} /> : <LeftSidearrowIncon />}
       </SideBarButton>
       <NavContainer>
         <LogoContainer>
-          <PrimaryLogo 
-              src="/images/logo.png"
-              width="60px"
-              height="60px"
-            />
+          <PrimaryLogo
+            src="/images/logo.png"
+            width="60px"
+            height="60px"
+          />
         </LogoContainer>
-        <Line/>
+        <Line />
         {sidebarItems.map((item) => (
-          <Link
-          style={{ textDecoration: "none", color: "inherit" }}
-          href={item.href}
-          key={item.name}
-        >
-          <NavItem
-            Collapsed={isCollapsed}
-            style={{
-              backgroundColor:
-                router.pathname === item.href ? "#10b981" : "",
-            }}
+          <button
+            style={{ textDecoration: "none", color: "inherit" }}
+            onClick={() => handleChangeContent(item.href)}
           >
-            <NavItemIcon Collapsed={isCollapsed}>{item.icon}</NavItemIcon>
-            {!isCollapsed && (
-              <SubTitleSideBarH3>{item.name}</SubTitleSideBarH3>
-            )}
-          </NavItem>
-        </Link>
+            <NavItem
+              Collapsed={isCollapsed}
+              style={{
+                backgroundColor:
+                  router.pathname === item.href ? "#10b981" : "",
+              }}
+            >
+              <NavItemIcon Collapsed={isCollapsed}>{item.icon}</NavItemIcon>
+              {!isCollapsed && (
+                <SubTitleSideBarH3>{item.name}</SubTitleSideBarH3>
+              )}
+            </NavItem>
+          </button>
         ))}
       </NavContainer>
       <UserContainer>
