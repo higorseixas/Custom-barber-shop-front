41be272718aded65babed2135e043c8bd42a2ed1
higorseixas <higor.c.seixas@gmail.com>
2023-04-27 18:12:55 -0300

    Alterando sidebar

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -8,68 +8,12 @@ import { PrimaryLogo } from '../Logo/primaryLogo';
 import { CalenderIcon, ClientIcon, ConfigIncon, LeftSidearrowIncon, MenuDashboardIcon, PaymentIcon, ServicesIcon } from '../Svgs';
 import { SubTitleSideBarH3 } from '../Titles/primaryTitles';
 import { Line, LogoContainer, NavContainer, NavItem, NavItemIcon, SidebarContainer, UserAvatar, UserContainer } from './styles';
+import { SidebarData } from './sidebarData';
 
 
 export default function Sidebar() {
   const router = useRouter();
   const { isCollapsed, toggleSidebarcollapse, handleSelectedContent } = useContext(SidebarContext);
-
-  const sidebarItems = [
-    {
-      name: "Home",
-      href: "dashboard",
-      icon: <MenuDashboardIcon style={{
-        marginRight: "10px",
-        height: "30px",
-        width: "30px"
-      }} />,
-    },
-    {
-      name: "Caixa",
-      href: "caixa",
-      icon: <PaymentIcon style={{
-        marginRight: "10px",
-        height: "30px",
-        width: "30px"
-      }} />,
-    },
-    {
-      name: "Agendamento",
-      href: "agendamento",
-      icon: <CalenderIcon style={{
-        marginRight: "10px",
-        height: "30px",
-        width: "30px"
-      }} />,
-    },
-    {
-      name: "Clientes",
-      href: "clientes",
-      icon: <ClientIcon style={{
-        marginRight: "10px",
-        height: "30px",
-        width: "30px"
-      }} />,
-    },
-    {
-      name: "Produtos &\n Serviços",
-      href: "produtosEServiços",
-      icon: <ServicesIcon style={{
-        marginRight: "10px",
-        height: "30px",
-        width: "30px"
-      }} />,
-    },
-    {
-      name: "Configurações",
-      href: "configurações",
-      icon: <ConfigIncon style={{
-        marginRight: "10px",
-        height: "30px",
-        width: "30px"
-      }} />,
-    },
-  ];
 
   const handleChangeContent = (content: string) => {
     handleSelectedContent(content)
@@ -87,9 +31,9 @@ export default function Sidebar() {
             width="60px"
             height="60px"
           />
-        </LogoContainer>
+        </LogoContainer>s
         <Line />
-        {sidebarItems.map((item) => (
+        {SidebarData.map((item) => (
           <button
             style={{ textDecoration: "none", color: "inherit" }}
             onClick={() => handleChangeContent(item.href)}
---
+++ src/components/Sidebar/sidebarData.tsx
@@ -0,0 +1,66 @@
+import { 
+  CalenderIcon, 
+  ClientIcon, 
+  ConfigIncon, 
+  MenuDashboardIcon, 
+  PaymentIcon,
+  ServicesIcon 
+} from '../Svgs';
+
+
+export const SidebarData = [
+  {
+    name: "Home",
+    href: "dashboard",
+    icon: <MenuDashboardIcon style={{
+      marginRight: "10px",
+      height: "30px",
+      width: "30px"
+    }} />,
+  },
+  {
+    name: "Caixa",
+    href: "caixa",
+    icon: <PaymentIcon style={{
+      marginRight: "10px",
+      height: "30px",
+      width: "30px"
+    }} />,
+  },
+  {
+    name: "Agendamento",
+    href: "agendamento",
+    icon: <CalenderIcon style={{
+      marginRight: "10px",
+      height: "30px",
+      width: "30px"
+    }} />,
+  },
+  {
+    name: "Clientes",
+    href: "clientes",
+    icon: <ClientIcon style={{
+      marginRight: "10px",
+      height: "30px",
+      width: "30px"
+    }} />,
+  },
+  {
+    name: "Produtos &\n Serviços",
+    href: "produtosEServiços",
+    icon: <ServicesIcon style={{
+      marginRight: "10px",
+      height: "30px",
+      width: "30px"
+    }} />,
+  },
+  {
+    name: "Configurações",
+    href: "configurações",
+    icon: <ConfigIncon style={{
+      marginRight: "10px",
+      height: "30px",
+      width: "30px"
+    }} />,
+  },
+];
