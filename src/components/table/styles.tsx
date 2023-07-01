ad70ac8ddb4c8ed1a6c7a94b77bb7b87ebf73a39
Higor Clem Seixas <higor.c.seixas@gmail.com>
2023-07-01 18:44:44 -0300

    Feat

--- src/components/table/styles.tsx
+++ src/components/table/styles.tsx
@@ -1,85 +1,39 @@
 import styled from 'styled-components';
 
-// export const SyledTable = styled.table`
-//   width: 100%;
-//   border-collapse: collapse;
-//   overflow: hidden;
-//   border: 1px solid #000000;
-//   border-radius: 0.5rem;
-//   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
-//   th {
-//     text-align: left;
-//     padding: 1rem;
-//   }
-// `;
-
-// export const SyledTableHeader = styled.thead`
-//   color: #fff;
-//   background-image: linear-gradient(to right, #6c63ff, #a855f7);
-// `;
-
-// export const SyledTableBody = styled.tbody`
-//   & > tr:nth-child(even) {
-//     background-color: #f3f3f3;
-//     :hover {
-//       background-color: #e4dddd;
-//     }
-//   }
-
-//   & > tr:nth-child(odd) {
-//     background-color: #ffffff;
-//     :hover {
-//       background-color: #e4dddd;
-//     }
-//   }
-
-// `;
-
-// export const StyledTableCell = styled.td`
-//   display: flex;
-//   justify-content: center;
-// `;
-
-// export const StyledButton = styled.button`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   margin: 1rem;
-//   padding: 0.5rem;
-//   border-radius: 9999px;
-//   color: ${({ color }) => color};
-//   background-color: ${({ backgroundColor }) => backgroundColor};
-//   &:hover {
-//     background-color: ${({ hoverBackgroundColor }) => hoverBackgroundColor};
-//   }
-// `;
-
 export const TableContainer = styled.table`
   width: 100%;
+  height: 50%;
   border-collapse: collapse;
 `;
 
 export const TableHeader = styled.th`
-  padding: 8px;
-  text-align: left;
+  padding: 5px;
+  text-align: center;
   border-bottom: 1px solid #ddd;
+`;
+
+export const TableRowWrapper = styled.div`
+  height: 300px;
+  overflow-y: scroll;
 `;
 
 export const TableRow = styled.tr`
   &:nth-child(even) {
     background-color: #f2f2f2;
   }
+  &:hover {
+    background-color: #bbb2b2;
+  }
 `;
 
 export const TableCell = styled.td`
-  padding: 8px;
+  padding: 1px;
   border-bottom: 1px solid #ddd;
+  text-align: center;
 `;
 
 export const ActionsCell = styled(TableCell)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  text-align: right;
 `;
 
 export const EditButton = styled.button`
--- src/templates/Services/index.tsx
+++ src/templates/Services/index.tsx
@@ -1,17 +1,50 @@
 import CustomTable from "../../components/table/index";
 
 export default function Services() {
-  const headers = ['Nome', 'Idade', 'Email'];
+
   const data = [
     ['João', 25, 'joao@example.com'],
     ['Maria', 30, 'maria@example.com'],
     ['Pedro', 40, 'pedro@example.com'],
-  ];  
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+    ['Pedro', 40, 'pedro@example.com'],
+  ];
+  
+  const handleEdit = (item: any) => {
+    console.log(item)
+  }
+
   return (
-    <CustomTable 
-      headers={headers}
-      data={data}
-      displayActions={false}
-    />
+    <>
+      <div style={{ height: '400px' }}>
+        <CustomTable 
+          columns={[
+            { label: 'Name', minWidth: 170 },
+            { label: 'ISO\u00a0Code', minWidth: 100 },
+            {
+              label: 'Density',
+              minWidth: 170,
+              // align: 'right',
+              format: (value: number) => value.toFixed(2),
+            },
+          ]}
+          data={data}
+          displayActions={true}
+          handleEdit={handleEdit}
+          handleDelete={handleEdit}
+        />
+      </div>
+
+      <p>TESTE</p>
+    </>
   );
 };
