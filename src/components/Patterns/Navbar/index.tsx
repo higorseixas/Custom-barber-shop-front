c49ef2e37837956b73251daaf632f40c54ab6f19
Your Name <leonardorocha.limaa@gmail.com>
2023-07-01 17:21:42 -0300

    Fixing the space on NavContainer items

--- src/components/Patterns/Navbar/index.tsx
+++ src/components/Patterns/Navbar/index.tsx
@@ -4,17 +4,17 @@ import { faClock, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import { PrimaryButton } from '../../buttons/Button';
 import { PrimaryLogo } from '../../logo/PrimaryLogo';
-import { NavbarButtonContainer, NavbarContainer, StyledIcon, VerticalLine } from './styles';
+import { LinkDiv, NavbarButtonContainer, NavbarContainer, StyledIcon, VerticalLine } from './styles';
 
 
 export const NavBar = () => {
   return (
     <NavbarContainer>
-      <PrimaryLogo 
-      src="/images/logo.png" 
-      alt="Logo"
-      width="70px"
-      height="70px"/>
+      <PrimaryLogo
+        src="/images/logo.png"
+        alt="Logo"
+        width="70px"
+        height="70px" />
 
       <NavbarButtonContainer>
         <PrimaryButton span={{ marginLeft: "10px" }}>
@@ -31,12 +31,14 @@ export const NavBar = () => {
       </NavbarButtonContainer>
 
       <VerticalLine />
-      <Link href={'/login'}>
-        <PrimaryButton span={{ marginLeft: "10px" }}>
-          <StyledIcon icon={faUser} />
-          <span>Login</span>
-        </PrimaryButton>
-      </Link>
+      <LinkDiv>
+        <Link href={'/login'}>
+          <PrimaryButton span={{ marginLeft: "10px" }}>
+            <StyledIcon icon={faUser} />
+            <span>Login</span>
+          </PrimaryButton>
+        </Link>
+      </LinkDiv>
     </NavbarContainer>
   );
 };
